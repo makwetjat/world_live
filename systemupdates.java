c0a3cd7c17953c989a796934cca639f79043e305
makwetjat <70511068+makwetjat@users.noreply.github.com>
2022-10-26 21:59:11 +0200

    Update systemupdates.java

--- systemupdates.java
+++ systemupdates.java
@@ -1,7 +1,17 @@
-public static void main(String... args) {
-    Main main = new Main();
+String command = "ping www.codejava.net";
  
-    //test command in linux
-    main.runCommand("pwd");
-    main.runCommand("ifconfig", "-a");
+try {
+    Process process = Runtime.getRuntime().exec(command);
+ 
+    BufferedReader reader = new BufferedReader(
+            new InputStreamReader(process.getInputStream()));
+    String line;
+    while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+    }
+ 
+    reader.close();
+ 
+} catch (IOException e) {
+    e.printStackTrace();
 }
