19e77b9dc4c6d4880c564337b5deebbc6e5a185f
makwetjat <70511068+makwetjat@users.noreply.github.com>
2022-10-26 21:42:10 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,13 +5,7 @@ class HelloWorld
 {
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
-    public static void main(String args[])
-    {
-        System.out.println("Hello, World");
-    }
-}
-
-public static void main(String... args) {
+ public static void main(String... args) {
     Main main = new Main();
  
     //test command in linux
